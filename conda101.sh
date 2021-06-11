126cfd5697bb33b671a3cde2831c8d1033e87938
omegadan01 <dbruzzes@nd.edu>
2021-06-11 15:14:41 -0400

    conda update

--- conda101.sh
+++ conda101.sh
@@ -7,7 +7,7 @@
 conda info --env 
 
 # remove installed enviroment 
-conda env remove --name foo
+conda env remove --name foo --all
 
 # create new enviroment 
 conda create --name foo
@@ -22,3 +22,14 @@ conda deactivavte
 conda activate foo
 conda install -c bioconda bar 
 
+#################
+# create a python 2.7 enviroment and install some programs 
+conda create --name py2 python=2.7
+conda activate py2
+
+conda install -c bioconda trimmomatic
+conda install -c bioconda fastqc
+
+# test to see the programs run by running them 
+fastqc -h 
+trimmomatic -h
