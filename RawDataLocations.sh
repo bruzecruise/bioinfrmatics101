3a1afede3055840269c0ac2d473f7700720f166c
Dan Bruzzese <31770882+bruzecruise@users.noreply.github.com>
2023-07-28 01:11:36 -0400

    Create RawDataLocations.sh
    file locations

---
+++ RawDataLocations.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## the raw files are located below 
+
+# location of May 2023 sequencing run on the novaseq
+# data for Dan Michelle Andrew Issac and Parasitoids 
+cd /afs/crc.nd.edu/group/rhago/osmanthi/CMG_DanAndrewMichelle2023
+
+
+
+
+####################### walnut chaos ###############################
+############### run1 lane 1/2
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/MMG_1_2/FCC7AEEACXX-CHKPE15040018_L3_1.fq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/MMG_1_2/FCC7AEEACXX-CHKPE15040018_L3_2.fq.gz
+
+#run 1 lane 3/4
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/MMG_3_4/FCC7AEEACXX-CHKPE15040019_L4_1.fq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/MMG_3_4/FCC7AEEACXX-CHKPE15040019_L4_2.fq.gz
+
+# run 1 original barcodes 
+# 192
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/BarcodeFiles/barcodes.MMG.1.2.txt
+# 192
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/BGI_Mar_2015/BarcodeFiles/barcodes.MMG.3.4.txt
+
+
+############## run2 2017 
+# run2 lane 1
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L001_R1_001.fastq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L001_R2_001.fastq.gz
+
+# run2 lane 2
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L002_R1_001.fastq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L002_R2_001.fastq.gz
+
+# run2 lane 3
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L003_R1_001.fastq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L003_R2_001.fastq.gz
+
+# run2 lane 4
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L004_R1_001.fastq.gz
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/MMg1-2-3_S1_L004_R2_001.fastq.gz
+
+# run2 original barcodes = 239 samples ? = 1 file
+# /afs/crc.nd.edu/user/d/dbruzzes/rhago/cingulata/HiSeqRawData/ND_NextSeq/MMG123/barcodes.MMG123.2017.txt
